606302642079607cf57db40467f160b5f0659086
Your Name <you@example.com>
2020-09-04 16:33:13 +0800

    完成前台和中台 的开发 即将进行后台开发

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -9,7 +9,7 @@ class HomeController extends Controller {
     // eslint-disable-next-line indent
   }
   async getArticleList() {
-    const sql = 'select article.id,typeName,title,article_content,introduce,addTime,view_count from article, type where article.id=type.id';
+    const sql = 'select article.id,typeName,title,article_content,introduce,addTime,view_count from article, type where article.type_id=type.id';
     const result = await this.app.mysql.query(sql);
     this.ctx.body = {
       data: result,
@@ -18,7 +18,23 @@ class HomeController extends Controller {
   // eslint-disable-next-line no-empty-function
   async getArticleById() {
     const id = this.ctx.params.id;
-    const sql = 'select article.id,typeName,title,article_content,introduce,addTime,view_count from article, type where article.id=type.id and article.id = ' + id;
+    const sql = 'select article.id,article.type_id as type_id,typeName,title,article_content,introduce,addTime,view_count from article, type where article.type_id=type.id and article.id = ' + id;
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: result,
+    };
+  }
+
+  // 获取type 列表 名称和 编号
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type');
+    this.ctx.body = { data: result };
+  }
+
+  // 根据 不同的 类型获取 文章
+  async getListByTypeId() {
+    const type_id = this.ctx.params.type_id;
+    const sql = 'select article.id,typeName,title,article_content,introduce, addTime,view_count from article, type where article.type_id=type.id and article.type_id = ' + type_id;
     const result = await this.app.mysql.query(sql);
     this.ctx.body = {
       data: result,
--- app/router/default.js
+++ app/router/default.js
@@ -4,5 +4,7 @@ module.exports = app => {
   router.get('/default/index', controller.default.home.index);
   router.get('/default/getArticleList', controller.default.home.getArticleList);
   router.get('/default/getArticleById/:id', controller.default.home.getArticleById);
+  router.get('/default/getTypeInfo', controller.default.home.getTypeInfo);
+  router.get('/default/getListByTypeId/:type_id', controller.default.home.getListByTypeId);
   // eslint-disable-next-line eol-last
 };
